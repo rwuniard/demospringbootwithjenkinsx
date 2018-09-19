001fbc46055d5c685f7a2819aab71b2744b631fb
Ronson Wuniardy <rwuniard@hotmail.com>
2018-09-19 19:11:17 -0400

    Initial commit

---
+++ src/main/java/com/example/demospringbootwithjenkinsx/DemospringbootwithjenkinsxApplication.java
@@ -0,0 +1,12 @@
+package com.example.demospringbootwithjenkinsx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemospringbootwithjenkinsxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemospringbootwithjenkinsxApplication.class, args);
+    }
+}
---
+++ src/main/java/com/example/demospringbootwithjenkinsx/HelloWorld.java
@@ -0,0 +1,23 @@
+package com.example.demospringbootwithjenkinsx;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="helloworld")
+public class HelloWorld {
+
+    @RequestMapping(value="printHello", method= RequestMethod.GET)
+    public String helloWolrd() {
+        return "Hello World!";
+    }
+
+    @RequestMapping(value="printMessage/{message}", method = RequestMethod.GET)
+    public String print(@PathVariable String message) {
+        return "The message is: " + message;
+    }
+
+
+}
